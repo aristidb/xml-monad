74e01b89fe8eb0213c6fcecf6cf07a9224ae320c
Aristid Breitkreuz <aristidb@googlemail.com>
2010-10-20 22:41:26 +0200

    Text.XML.Monad.Input: specially handle empty document errors

--- Text/XML/Monad/Input.hs
+++ Text/XML/Monad/Input.hs
@@ -4,6 +4,7 @@ module Text.XML.Monad.Input
 where
   
 import           Text.XML.Monad.Core
+import           Data.Maybe
 import           MonadLib
 import qualified Text.XML.Light       as L
 import qualified Text.XML.Light.Lexer as LL
@@ -12,4 +13,7 @@ parseXML :: (ReaderM m s, LL.XmlSource s) => m [L.Content]
 parseXML = asks L.parseXML
 
 parseXMLDoc :: (ReaderM m s, ExceptionM m ParseError, LL.XmlSource s) => m L.Element
-parseXMLDoc = asksMaybe InvalidXml L.parseXMLDoc
+parseXMLDoc = do
+  x <- ask
+  when (isNothing $ LL.uncons x) $ raise EmptyDocument
+  maybeRaise InvalidXml $ L.parseXMLDoc x
